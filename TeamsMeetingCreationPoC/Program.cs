1ad56f169703b7c5df7e4b6b73a8edaf19e94206
mmsharepoint <markus.moeller.ed@outlook.de>
2023-09-30 16:37:53 +0200

    Initial commit

---
+++ TeamsMeetingCreationPoC/Program.cs
@@ -0,0 +1,68 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Client;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using TeamsMeetingCreationPoC.controller;
+using System.Net.Http.Headers;
+
+var builder = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json");
+var config = builder.Build();
+
+string clientId = config["AZURE_CLIENT_ID"];
+string tenantId = config["AZURE_TENANT_ID"];
+string clientSecret = config["AZURE_CLIENT_SECRET"];
+string userPrincipalName = "markus@mmoellermvp.onmicrosoft.com";
+string dummyAttendee = "cclausen@mmoellermvp.onmicrosoft.com";
+
+string customerName = "Contoso";
+string customerEmail = "BenBenson@contoso.com";
+string customerPhone = "+491515445556";
+string customerId = "47110815";
+
+GraphController graphController = new GraphController(tenantId, clientId, clientSecret);
+
+string meetingSubject = "Test Meeting with App/Tab 4";
+//OnlineMeeting om = new OnlineMeeting
+//{
+//    Subject = meetingSubject,
+//    StartDateTime = DateTime.Now,
+//    EndDateTime = DateTime.Now.AddHours(1)
+//};
+string userID = await graphController.GetUserId(userPrincipalName);
+string joinUrl = await graphController.CreateTeamsMeeting(userID, userPrincipalName, dummyAttendee, meetingSubject);
+
+//var newOM = await graphClient
+//        .Users[userID].OnlineMeetings.PostAsync(om);
+//Console.WriteLine($"Online Meeting created with ID {newOM.Id}");
+//Console.WriteLine($"Event created with JoinUrl {newOM.JoinWebUrl}");
+//string joinUrl = newOM.JoinWebUrl;
+// string joinUrl = "https://teams.microsoft.com/l/meetup-join/19%3ameeting_NTFhNDc2NjktMmJkMi00ZGMxLWJkNDYtNDNiZWQ3YTI2NGRh%40thread.v2/0?context=%7b%22Tid%22%3a%227e77d071-ed08-468a-bc75-e8254ba77a21%22%2c%22Oid%22%3a%226c76948a-7520-4c33-bf37-b1a39b78859f%22%7d";
+// !!! https://learn.microsoft.com/en-us/graph/cloud-communication-online-meeting-application-access-policy
+
+// HttpClientController clientController = new HttpClientController(accessToken);
+// string onlineMeetingResult = await clientController.GetOnlineMeeting(userID, joinUrl);
+// Console.WriteLine($"OnlineMeeting /n {onlineMeetingResult}");
+string chatId = await graphController.GetMeetingChatId(userID, joinUrl);
+
+string appId = await graphController.GetAppId();
+if (appId != "")
+{
+    bool appInstalled = await graphController.InstallAppInChat(appId, chatId);
+    if (appInstalled)
+    {
+        await graphController.InstallTabInChat(appId, chatId);
+    }
+}
+AzureController azrCtrl = new AzureController(config);
+azrCtrl.storeConfigValue($"TEAMSMEETINGSERVICECALL:{chatId}:CUSTOMERNAME", customerName);
+azrCtrl.storeConfigValue($"TEAMSMEETINGSERVICECALL:{chatId}:CUSTOMERPHONE", customerPhone);
+azrCtrl.storeConfigValue($"TEAMSMEETINGSERVICECALL:{chatId}:CUSTOMEREMAIL", customerEmail);
+azrCtrl.storeConfigValue($"TEAMSMEETINGSERVICECALL:{chatId}:CUSTOMERID", customerId);
+Console.WriteLine(azrCtrl.GetConfigurationValue($"TEAMSMEETINGSERVICECALL:{chatId}:CUSTOMERNAME") ?? "No customername");
+Console.WriteLine(azrCtrl.GetConfigurationValue($"TEAMSMEETINGSERVICECALL:{chatId}:CUSTOMERPHONE") ?? "No customerphone");
+Console.WriteLine(azrCtrl.GetConfigurationValue($"TEAMSMEETINGSERVICECALL:{chatId}:CUSTOMEREMAIL") ?? "No customeremail");
+Console.WriteLine(azrCtrl.GetConfigurationValue($"TEAMSMEETINGSERVICECALL:{chatId}:CUSTOMERID") ?? "No customerid");
+Console.ReadLine();
---
+++ TeamsMeetingServiceCall/Controllers/GraphController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
+using Microsoft.Identity.Web;
+using TeamsMeetingServiceCall.controller;
+using TeamsMeetingServiceCall.Models;
+
+namespace TeamsMeetingServiceCall.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GraphController : ControllerBase
+    {
+        private readonly GraphServiceClient _graphClient;
+        private readonly ITokenAcquisition _tokenAcquisition;
+        private readonly ILogger<GraphController> _logger;
+        public GraphController(ITokenAcquisition tokenAcquisition, GraphServiceClient graphClient, ILogger<GraphController> logger)
+        {
+            _tokenAcquisition = tokenAcquisition;
+            _graphClient = graphClient;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<string>> Get(string meetingId)
+        {
+            _logger.LogInformation($"MeetingID: {meetingId}");
+            string chatId = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(meetingId));
+            _logger.LogInformation($"MeetingID decoded: {chatId.Replace("0#", "").Replace("#0", "")}");
+            string plainChatId = chatId.Replace("0#", "").Replace("#0", "");
+            Chat chat = await this._graphClient.Chats[plainChatId].Request().GetAsync();
+
+            _logger.LogInformation($"OrganizerID: {chat.OnlineMeetingInfo.Organizer.Id}");
+            _logger.LogInformation($"JoinWebUrl: {chat.OnlineMeetingInfo.JoinWebUrl}");
+            CustomerData result = await this.GetCustomMeetingData(plainChatId);
+            return Ok(result);
+        }
+
+        private async Task<string> GetAccessToken()
+        {
+            _logger.LogInformation($"Authenticated user: {User.GetDisplayName()}");
+
+            try
+            {
+                // TEMPORARY
+                // Get a Graph token via OBO flow
+                var token = await _tokenAcquisition
+                    .GetAccessTokenForUserAsync(new[]{
+                        "Files.ReadWrite", "Sites.ReadWrite.All" });
+
+                // Log the token
+                _logger.LogInformation($"Access token for Graph: {token}");
+                return token;
+            }
+            catch (MicrosoftIdentityWebChallengeUserException ex)
+            {
+                _logger.LogError(ex, "Consent required");
+                // This exception indicates consent is required.
+                // Return a 403 with "consent_required" in the body
+                // to signal to the tab it needs to prompt for consent
+                return "";
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred");
+                return "";
+            }
+        }
+
+        private async Task<CustomerData> GetCustomMeetingData(string meetingId)
+        {
+            AzureController azrCtrl = new AzureController("Endpoint=https://mmoteamsconfiguration.azconfig.io;Id=/nk1-l9-s0:JJRg85Y6Y+GzQ1rRLdzf;Secret=wX+jBL/p0WwB/3Z8SGVP8rMsQ8t1DcZC+te5wK84nuw="); // ToDo!!         
+            string customerName = azrCtrl.GetConfigurationValue($"TEAMSMEETINGSERVICECALL:{meetingId}:CUSTOMERNAME") ?? "No customername";
+            string customerPhone = azrCtrl.GetConfigurationValue($"TEAMSMEETINGSERVICECALL:{meetingId}:CUSTOMERPHONE") ?? "No customerphone";
+            string customerEmail = azrCtrl.GetConfigurationValue($"TEAMSMEETINGSERVICECALL:{meetingId}:CUSTOMEREMAIL") ?? "No customeremail";
+            string customerId = azrCtrl.GetConfigurationValue($"TEAMSMEETINGSERVICECALL:{meetingId}:CUSTOMERID") ?? "No customerid";
+
+            CustomerData customerData = new CustomerData
+            {
+                Name = customerName,
+                Phone = customerPhone,
+                Email = customerEmail,
+                Id = customerId
+            };
+            return customerData;
+        }
+    }
+}
